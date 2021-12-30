e7e9e2a2e8bcc05f9a626218cf546f57becfc6e4
shaunaloh <57122906+shaunaloh@users.noreply.github.com>
2021-12-30 11:08:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 OUTPUT_FOLDER=data/SVO_triples/
 IFS="/"
-for FILE in data/raw_21Dec/* 
+for FILE in data/raw_DOCRED/* 
 do 
   read -ra ADDR <<<"$FILE"
   python knowledge_graph.py --text_file "$FILE" --output_file "${OUTPUT_FOLDER}${ADDR[-1]}"
